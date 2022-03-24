467ff9a5e203fa51acbda397901e1292c188c42c
jinyongkwon <wlsdyd1178@naver.com>
2022-03-24 09:22:42 +0900

    1.이니셜 프로젝트

---
+++ src/test/java/site/metacodig/dbtest/DbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacodig.dbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
